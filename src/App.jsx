53b02b1055914291f6e3762b76d034b757bb7472
MahadevaSneha <snehamahadeva510@gmail.com>
2024-05-13 16:08:07 +0530

    Commit message

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './Editor.css'
+import Editor from './components/Editor'
+function App() {
+  return (
+    <div className='App'>
+      <Editor/>
+    </div>
+  )
+   
+}
+
+export default App;
